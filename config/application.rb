b259f76cb21cce760c40f2b9baad95826bd8526d
Wes Gamble <weyus@att.net>
2013-08-14 20:54:47 -0500

    Try to get Facebook page to show Healthpost site in iframe.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module HealthpostFacebook
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOW-FROM https://www.facebook.com'
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
