2847bd0a3d207eb7f2f2daeee8f45dddbcde0b80
Wes Gamble <weyus@att.net>
2013-08-14 20:29:26 -0500

    Correct insertion of page_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       #Associate the Facebook page if a Facebook page association request comes in
       if current_user.provider_admin? && params[:tabs_added]
-        current_user.provider.associate_fb_page(params[:tabs_added].first.first)
+        current_user.provider.associate_fb_page(params[:tabs_added].keys.first)
       end
 
       redirect_target = if current_user.admin?
