f0c261abf32bc6a30f36008a65ad20349dccf0b0
Wes Gamble <weyus@att.net>
2013-08-14 19:54:27 -0500

    Bug fix.

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -9,8 +9,7 @@ class Provider < ActiveRecord::Base
   delegate :scss_filename=, to: :provider_display
 
   def associate_fb_page(page_id)
-    provider_facebook_page ||= create_provider_facebook_page
-    provider_facebook_page.update_attribute(:fb_page_id, page_id)
+    provider_facebook_page ? provider_facebook_page.update_attribute(:fb_page_id, page_id) : create_provider_facebook_page(fb_page_id: page_id)
   end
 end
 
