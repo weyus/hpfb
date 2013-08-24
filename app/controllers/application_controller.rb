fccef3b532a20241f6cae11d6e63fd2ffbb3041d
Wes Gamble <weyus@att.net>
2013-08-24 15:35:26 -0500

    Fix cookie keys and add comment re: protect_from_forgery behavior

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Could use with: null_session here and then wouldn't need to skip before filter in HealthpostController#facebook_page, but this feels more secure.
   protect_from_forgery with: :exception
 
   #The "signed_in" logic is necessary to avoid an endless redirection loop
@@ -38,11 +39,11 @@ class ApplicationController < ActionController::Base
   private
 
   def check_provider_id
-    (! cookies['page_id']) || (cookies['page_id'] == current_user.provider.fb_page_id)
+    (! cookies[:page_id]) || (cookies[:page_id] == current_user.provider.fb_page_id)
   end
 
   def check_provider_admin
-    (! cookies['page_admin']) || (cookies['page_admin'] == current_user.provider_admin?.to_s)
+    (! cookies[:page_admin]) || (cookies[:page_admin] == current_user.provider_admin?.to_s)
   end
 end
 
